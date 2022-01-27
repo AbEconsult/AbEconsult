1c760688e47e12d583814adf3015d027ac7c8316
Elabjani Abel <commercial@econsult-online.fr>
2022-01-27 20:27:24 +0100

    Projet series

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountPasswordController extends AbstractController
 {
-    #[Route('/account/password', name: 'account_password')]
-    public function index(): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('account_password/index.html.twig', [
-            'controller_name' => 'AccountPasswordController',
-        ]);
+        $this -> entityManager = $entityManager;
+    }
+
+    /**
+     * @param Request $request
+     * @param UserPasswordHasherInterface $encoder
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/compte/modifier-mon-mot-de-passe', name: 'account_password')]
+    public function index(Request $request, UserPasswordHasherInterface $encoder, EntityManagerInterface $manager): Response
+    {
+        $notification = null;
+
+        $user   =   $this ->getUser();
+        $form = $this->createForm(ChangePasswordType::class,$user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $old_pwd = $form->get('old_password')->getData();
+            if ($encoder->isPasswordValid($user, $old_pwd)){
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->hashPassword($user, $new_pwd);
+
+                $user->setPassword($password);
+//                $manager->persist($user); je prépare et je fige la donnée pour la création
+ 	            $manager->flush();
+                 $notification = "votre mot de passe a bien été modifié";
+// 	            $this -> addFlash('succes', 'votre mot de passe a bien été modifié’);
+
+            }
+        }
+
+        return $this->render('account/password.html.twig', [
+            'form'=>$form->createView()
+         ]);
     }
 }
